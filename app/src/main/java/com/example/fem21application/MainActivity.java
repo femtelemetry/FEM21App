d771a61b505b7709244d9f5929e030e233c12adc
Caleb Johann <ccalebjohann@gmail.com>
2024-02-28 03:11:19 +0900

    Comments on TODO

--- app/src/main/java/com/example/fem21application/MainActivity.java
+++ app/src/main/java/com/example/fem21application/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //sets the initial layout to "activity_main.xml"
     }
+
+    //TODO: Make Bluetooth.java
+    //TODO: Copy and revise previous code
 }
