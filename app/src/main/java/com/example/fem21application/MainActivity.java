effa17bf5cac61dae93b8a14fbc94e80e916f1ad
Caleb Johann <ccalebjohann@gmail.com>
2024-02-28 03:26:13 +0900

    MA Planning

--- app/src/main/java/com/example/fem21application/MainActivity.java
+++ app/src/main/java/com/example/fem21application/MainActivity.java
@@ -3,14 +3,34 @@ package com.example.fem21application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TODO:Define 3 types of variables and initialize (e.g. view, status, flags): How to format?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //sets the initial layout to "activity_main.xml"
+        //Log layout
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); //keeps screen on
+
+        //receiver to receive messages from Bluetooth.java
+        //FindID
     }
 
     //TODO: Copy and revise previous code
+
+    //Formatting (previously used in the name valueChecker)
+
+    //on Restart
+    //on Destroy
+    //on Click
+    //on Acitivty result
+
+    //showMessage
+    //"RefindId"
+    //handler
 }
