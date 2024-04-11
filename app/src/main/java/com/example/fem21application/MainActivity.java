04f3b29cc52add93841dc70ba22116cddd2f0b1e
NIRUN-Weerawit <nirun.weerawit@gmail.com>
2024-04-11 16:53:36 +0900

    New update: added signalling button for keeping the app always connects to the database by signalling a number every 10 seconds

--- app/src/main/java/com/example/fem21application/MainActivity.java
+++ app/src/main/java/com/example/fem21application/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.fem21application;
 
-import static java.text.DateFormat.DATE_FIELD;
-import static java.text.DateFormat.getDateInstance;
-import static java.text.DateFormat.getTimeInstance;
+
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,13 +22,15 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.SetOptions;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MainActivity extends AppCompatActivity {
     String title = "FEM21App data"; //Title of a directory [Realtime database]
@@ -40,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance(); //Connect to Cloud FireStore.
     FirebaseDatabase database = FirebaseDatabase.getInstance("https://fem21app-f43ef-default-rtdb.asia-southeast1.firebasedatabase.app/"); //connect to the Realtime Database
     String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date()); // Create a SimpleDateFormat object with the desired date format
-
+    int signalFlag = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +56,33 @@ public class MainActivity extends AppCompatActivity {
         //Log layout
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); //keeps screen on
+
+        //To continuously send signal to database to keep connecting to it.
+        AtomicReference<ScheduledExecutorService> executor = new AtomicReference<>(Executors.newScheduledThreadPool(1));
+        Button signallingBtn = findViewById(R.id.signallingButton);
+        signallingBtn.setOnClickListener(v -> {
+
+            if(signalFlag == 1){
+                signalFlag = 0;
+                signallingBtn.setText("Deactivate Signalling");
+                final int[] num = {0};
+                // Schedule the task to run every 30 seconds
+
+                executor.get().scheduleAtFixedRate(() -> {
+                    // Your method to be executed periodically
+                    num[0]++;
+                    RealFireStore(date,"0", num[0]);
+                    System.out.println("Executing task with signal: " + num[0]);
+                }, 0, 10, TimeUnit.SECONDS);
+            } else{
+                signallingBtn.setText("Activate Signalling");
+                signalFlag = 1;
+                executor.get().shutdown();
+                executor.set(Executors.newScheduledThreadPool(1));
+            }
+
+        });
+
 
         //To submit any input text to the databases
         Button submitButton = findViewById(R.id.SubmitButton);
@@ -71,6 +99,35 @@ public class MainActivity extends AppCompatActivity {
             Map<String, Object> data = new HashMap<>();  //For Cloud Firebase, the data needs to be of HashMap.
             data.put(time, text);
             CloudFireStore(date, data);
+            Toast.makeText(this, "The message is sent to the database...", Toast.LENGTH_SHORT).show();
+
+        });
+
+        //To run number infinitely
+        Button RunButton = findViewById(R.id.RunButton);
+        RunButton.setOnClickListener( v -> {
+            Log.i("database", "The data is being sent to the database");
+            for (int i = 0; i<=100; i++){
+                long nanoTime = System.nanoTime();
+                long micros = (nanoTime / 100000000)  ; // Extract microseconds from nanoseconds
+
+                String time = new SimpleDateFormat("HH:mm:ss:" + micros, Locale.getDefault()).format(new Date()); //Use timestamp as keys
+
+                //Map<String, Object> data = new HashMap<>();  //For Cloud Firebase, the data needs to be of HashMap.
+                //data.put(time, i);
+                //CloudFireStore(date, data);
+                //Random random = new Random();
+                // Generate a random integer between 0 and 120
+//                int randomNumber = (new Random()).nextInt(120);
+                RealFireStore(date, time, i);
+                Log.i("database", time + ":" + i);
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            Toast.makeText(this, "The number is running now...", Toast.LENGTH_SHORT).show();
         });
 
         //TextView Textbox = findViewById(R.id.TextBox2);
@@ -113,8 +170,9 @@ public class MainActivity extends AppCompatActivity {
     //Store data to Realtime Database
     private void RealFireStore(String folder,String key, Object data){
         // Write a message to the database
-        DatabaseReference myRef = database.getReference(title).child(folder);
+        DatabaseReference myRef = database.getReference(title + "/" + folder);
         myRef.child(key).setValue(data);
+        //Log.i("database", "Stored data");
     }
 
     //TODO: Fix RealFireRead
@@ -174,5 +232,11 @@ public class MainActivity extends AppCompatActivity {
                         Log.w(TAG, "Error getting documents.", task.getException());
                     }
                 });
+    }
+
+    private void DatabaseSignaling (int duration){
+        while (true) {
+
+        }
     }
 }
